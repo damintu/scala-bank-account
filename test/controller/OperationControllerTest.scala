c91eb87b215a59650472fc9f8f71cdeab425408e
Damien Sarramalho <d.sarramalho@gmail.com>
2019-01-24 22:56:47 +0100

    Adds withdrawal tests

--- test/controller/OperationControllerTest.scala
+++ test/controller/OperationControllerTest.scala
@@ -22,6 +22,7 @@ class OperationControllerTest {
       .Then()
       .statusCode(200)
       .body("$",hasKey("id"))
+      .body("$",hasKey("date"))
       .body("account",equalTo(1))
       .body("type",equalTo("deposit"))
       .body("amount",greaterThan(50))
@@ -48,6 +49,57 @@ class OperationControllerTest {
       .body("{" +
         " \"account\" : 1," +
         " \"type\" : \"deposit\"," +
+        "\"amount\" : 0}")
+      .contentType(ContentType.JSON)
+      .accept(ContentType.JSON)
+      //@TODO path should be '/' but it's currently not working for me
+      .post("http://localhost:9000/operation")
+      .Then()
+      .statusCode(400)
+  }
+//Withdrawal test
+
+  @Test
+  def `withdrawal with positive amount `() {
+    given()
+      .body("{" +
+        " \"account\" : 1," +
+        " \"type\" : \"withdrawal\"," +
+        "\"amount\" : 50}")
+      .contentType(ContentType.JSON)
+      .accept(ContentType.JSON)
+      //@TODO path should be '/' but it's currently not working for me
+      .post("http://localhost:9000/operation")
+      .Then()
+      .statusCode(200)
+      .body("$",hasKey("id"))
+      .body("$",hasKey("date"))
+      .body("account",equalTo(1))
+      .body("type",equalTo("withdrawal"))
+      .body("amount",greaterThan(50))
+  }
+
+  @Test
+  def `withdrawal with negative amount`() {
+    given()
+      .body("{" +
+        " \"account\" : 1," +
+        " \"type\" : \"withdrawal\"," +
+        "\"amount\" : -50}")
+      .contentType(ContentType.JSON)
+      .accept(ContentType.JSON)
+      //@TODO path should be '/' but it's currently not working for me
+      .post("http://localhost:9000/operation")
+      .Then()
+      .statusCode(400)
+  }
+
+  @Test
+  def `withdrawal 0`() {
+    given()
+      .body("{" +
+        " \"account\" : 1," +
+        " \"type\" : \"withdrawal\"," +
         "\"amount\" : 0}")
       .contentType(ContentType.JSON)
       .accept(ContentType.JSON)
